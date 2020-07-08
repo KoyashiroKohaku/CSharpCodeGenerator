685e79e292cee54408596b906fcbd2dff2c53860
Koyashiro Kohaku <develop@koyashiro.net>
2020-07-08 21:31:43 +0900

    test: update TypeResolverTest

--- tests/CSharpCodeGenerator.Test/TypeResolverTest.cs
+++ tests/CSharpCodeGenerator.Test/TypeResolverTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 
 namespace KoyashiroKohaku.CSharpCodeGenerator.Test
@@ -7,7 +8,7 @@ namespace KoyashiroKohaku.CSharpCodeGenerator.Test
     public class TypeResolverTest
     {
         [TestMethod]
-        public void GetTypeAliasTest()
+        public void GetTypeString()
         {
             Assert.AreEqual("bool", TypeResolver.GetTypeString(typeof(bool)));
             Assert.AreEqual("byte", TypeResolver.GetTypeString(typeof(byte)));
@@ -30,6 +31,117 @@ namespace KoyashiroKohaku.CSharpCodeGenerator.Test
             Assert.AreEqual("List<int>", TypeResolver.GetTypeString(typeof(List<int>)));
             Assert.AreEqual("List<string>", TypeResolver.GetTypeString(typeof(List<string>)));
             Assert.AreEqual("Dictionary<int, string>", TypeResolver.GetTypeString(typeof(Dictionary<int, string>)));
+        }
+
+        [TestMethod]
+        public void GetTypeAliasNameTest()
+        {
+            try
+            {
+                TypeResolver.GetTypeAliasName(typeof(List<>));
+
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("Specified argument was out of the range of valid values. (Parameter 'type')", e.Message);
+            }
+
+            Assert.AreEqual("bool", TypeResolver.GetTypeAliasName(typeof(bool)));
+            Assert.AreEqual("byte", TypeResolver.GetTypeAliasName(typeof(byte)));
+            Assert.AreEqual("sbyte", TypeResolver.GetTypeAliasName(typeof(sbyte)));
+            Assert.AreEqual("char", TypeResolver.GetTypeAliasName(typeof(char)));
+            Assert.AreEqual("decimal", TypeResolver.GetTypeAliasName(typeof(decimal)));
+            Assert.AreEqual("double", TypeResolver.GetTypeAliasName(typeof(double)));
+            Assert.AreEqual("float", TypeResolver.GetTypeAliasName(typeof(float)));
+            Assert.AreEqual("int", TypeResolver.GetTypeAliasName(typeof(int)));
+            Assert.AreEqual("uint", TypeResolver.GetTypeAliasName(typeof(uint)));
+            Assert.AreEqual("long", TypeResolver.GetTypeAliasName(typeof(long)));
+            Assert.AreEqual("ulong", TypeResolver.GetTypeAliasName(typeof(ulong)));
+            Assert.AreEqual("short", TypeResolver.GetTypeAliasName(typeof(short)));
+            Assert.AreEqual("ushort", TypeResolver.GetTypeAliasName(typeof(ushort)));
+            Assert.AreEqual("string", TypeResolver.GetTypeAliasName(typeof(string)));
+            Assert.AreEqual("object", TypeResolver.GetTypeAliasName(typeof(object)));
+            Assert.AreEqual("void", TypeResolver.GetTypeAliasName(typeof(void)));
+        }
+
+        [TestMethod]
+        public void TryGetTypeAliasName()
+        {
+            Assert.IsFalse(TypeResolver.TryGetTypeAliasName(null, out string actual));
+            Assert.AreEqual(string.Empty, actual);
+
+            Assert.IsFalse(TypeResolver.TryGetTypeAliasName(typeof(List<>), out actual));
+            Assert.AreEqual(string.Empty, actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(bool), out actual));
+            Assert.AreEqual("bool", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(byte), out actual));
+            Assert.AreEqual("byte", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(sbyte), out actual));
+            Assert.AreEqual("sbyte", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(char), out actual));
+            Assert.AreEqual("char", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(decimal), out actual));
+            Assert.AreEqual("decimal", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(double), out actual));
+            Assert.AreEqual("double", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(float), out actual));
+            Assert.AreEqual("float", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(int), out actual));
+            Assert.AreEqual("int", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(uint), out actual));
+            Assert.AreEqual("uint", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(long), out actual));
+            Assert.AreEqual("long", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(ulong), out actual));
+            Assert.AreEqual("ulong", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(short), out actual));
+            Assert.AreEqual("short", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(ushort), out actual));
+            Assert.AreEqual("ushort", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(string), out actual));
+            Assert.AreEqual("string", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(object), out actual));
+            Assert.AreEqual("object", actual);
+
+            Assert.IsTrue(TypeResolver.TryGetTypeAliasName(typeof(void), out actual));
+            Assert.AreEqual("void", actual);
+        }
+
+        [TestMethod]
+        public void ExistsTypeAliasTest()
+        {
+            Assert.IsFalse(TypeResolver.ExistsTypeAlias(null));
+            Assert.IsFalse(TypeResolver.ExistsTypeAlias(typeof(List<>)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(sbyte)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(char)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(decimal)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(double)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(float)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(int)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(uint)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(long)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(ulong)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(short)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(ushort)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(string)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(object)));
+            Assert.IsTrue(TypeResolver.ExistsTypeAlias(typeof(void)));
         }
     }
 }
