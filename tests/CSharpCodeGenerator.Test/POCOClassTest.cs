14e026439705c88989d79d3f09466253bc9abb2a
Koyashiro Kohaku <develop@koyashiro.net>
2020-07-09 19:00:21 +0900

    test: add POCOClassTest

---
+++ tests/CSharpCodeGenerator.Test/POCOClassTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KoyashiroKohaku.CSharpCodeGenerator.Test
+{
+    [TestClass]
+    public class POCOClassTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            try
+            {
+                new POCOClass(null);
+
+                Assert.Fail();
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.AreEqual("Value cannot be null. (Parameter 'className')", e.Message);
+            }
+
+            var pocoClass = new POCOClass("TestClass");
+
+            Assert.AreEqual("TestClass", pocoClass.ClassName);
+        }
+    }
+}
