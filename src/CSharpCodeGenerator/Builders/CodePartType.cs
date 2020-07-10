27d8b648381da0fe7f589cda02b64bc49f6ae7ae
Koyashiro Kohaku <develop@koyashiro.net>
2020-07-10 19:53:23 +0900

    feat: add CodePart and CodePartType

---
+++ src/CSharpCodeGenerator/Builders/CodePartType.cs
@@ -0,0 +1,11 @@
+namespace KoyashiroKohaku.CSharpCodeGenerator.Builders
+{
+    public enum CodePartType
+    {
+        Delimiter,
+        EndOfLine,
+        Indent,
+        ReservedWord,
+        AnyString
+    }
+}
